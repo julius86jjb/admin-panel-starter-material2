537b042a4276162a7d53b02017eb915495cfaaa0
Julio Jimenez <julius.1986@gmail.com>
2023-11-11 00:06:34 +0000

    env2

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environments = {
   // baseUrl: 'http://localhost:3000'
-  baseUrl:'https://admin-nest-backend-qkvil18dc-julio-jimenezs-projects.vercel.app/'
+  baseUrl:'https://admin-nest-backend-qkvil18dc-julio-jimenezs-projects.vercel.app'
 }
